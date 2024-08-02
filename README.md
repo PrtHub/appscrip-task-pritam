c424fb6b61f40605696e0eac091536918e61a070
Pritam Ghosh <pri10145@gmail.com>
2024-08-02 14:30:39 +0530

    readme and metadata

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Appscrip Task - Pritam Ghosh
 
 This repository contains the code for a responsive product listing page, developed using Next.js and leveraging Server Side Rendering (SSR). The page fetches product data from [Fake Store API](https://fakestoreapi.com/) and includes various filtering and sorting options. The project is hosted on vercel for public access and evaluation.
+
+## Deployment
+
+The project is deployed on Vercel and can be accessed at: [Project URL](https://appscrip-task-pritam.vercel.app/)
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import ReduxProvider from "@/providers/ReduxProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Appscrip Task Pritam",
-  description: "Products page",
+  title: "Appscrip Task - Pritam Ghosh",
+  description: "A comprehensive product listing page built using Next.js with Server Side Rendering (SSR). The page features responsive design, dynamic filtering and sorting options, and is optimized for SEO. View and interact with a curated selection of products, leveraging a mock API for data retrieval.",
 };
 
 export default function RootLayout({
